7a9ffbb929e3ddb2c8b83f49f6186729d1e2270d
Justin Hong <jhong2009@gmail.com>
2018-05-16 15:37:06 -0700

    in makeToken.js: created a helper function that generates a token with a payload, secret, and options

---
+++ server/middleware/makeToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const secret = "that is what I shared yesterday lol";
+
+// helper function
+const makeToken = function makeToken(user) {
+	const timestamp = new Date().getTime();
+	const payload = {
+		sub: user._id, // subject - who the token refers to
+		iat: timestamp, // issued at - when token is made
+		username: user.username // name of user stored in token
+	};
+	const options = {
+		expiresIn: "24h"
+	};
+	return jwt.sign(payload, secret, options);
+};
+
+module.exports = makeToken;
